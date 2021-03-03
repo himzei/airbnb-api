03d61c171e8b11fcf629b563951cf2d6c78cf101
himzei <himzei@gmail.com>
2021-03-03 18:56:23 +0900

    2.1 create room part one

--- rooms/serializers.py
+++ rooms/serializers.py
@@ -2,7 +2,8 @@ from rest_framework import serializers
 from .models import Room
 from users.serializers import UserSerializer
 
-class RoomSerializer(serializers.ModelSerializer):
+
+class ReadRoomSerializer(serializers.ModelSerializer):
 
     user = UserSerializer()
 
@@ -10,3 +11,16 @@ class RoomSerializer(serializers.ModelSerializer):
         model = Room
         exclude = ("modified", )
 
+
+class WriteRoomSerializer(serializers.Serializer):
+    name = serializers.CharField(max_length=140)
+    address = serializers.CharField(max_length=140)
+    price = serializers.IntegerField()
+    beds = serializers.IntegerField(default=1)
+    lat = serializers.DecimalField(max_digits=10, decimal_places=6)
+    lng = serializers.DecimalField(max_digits=10, decimal_places=6)
+    bedrooms = serializers.IntegerField(default=1)
+    bathrooms = serializers.IntegerField(default=1)
+    check_in = serializers.TimeField(default="00:00:00")
+    check_out = serializers.TimeField(default="00:00:00")
+    instant_book = serializers.BooleanField(default=False)
