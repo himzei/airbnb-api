2e8989ba8f97b2811c3f80bde3101fae31873b16
himzei <himzei@gmail.com>
2021-03-04 09:14:57 +0900

    2.6 meview and user_detail

--- users/serializers.py
+++ users/serializers.py
@@ -1,7 +1,22 @@
-from rest_framework import serializers 
+from rest_framework import serializers
 from .models import User
 
-class UserSerializer(serializers.ModelSerializer):
+
+class RelatedUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = (
+            "username", 
+            "first_name", 
+            "last_name", 
+            "email", 
+            "avatar", 
+            "superhost"
+        )
+
+
+class ReadUserSerializer(serializers.ModelSerializer):
     class Meta: 
         model = User 
-        exclude = ("groups", "user_permissions", "password", "last_login", "is_superuser", "is_active", "is_staff", "date_joined", "favs")
+        exclude = ("groups", "user_permissions", "password", "last_login",
+                   "is_superuser", "is_active", "is_staff", "date_joined")
